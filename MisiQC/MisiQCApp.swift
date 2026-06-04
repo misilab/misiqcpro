3f9b2ccb0ccf38aa4391964db47dc740f2b93b81
Misiraca <contact@misiraca.com>
2026-06-04 21:57:52 +0200

    Initial Commit

---
+++ MisiQC/MisiQCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisiQCApp.swift
+//  MisiQC
+//
+//  Created by Matthieu Misiraca on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MisiQCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
